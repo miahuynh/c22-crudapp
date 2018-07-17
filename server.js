e3cfa4867951a00f7de90ccb762f6763f31c84c0
Mia <ngocmhuynh5@gmail.com>
2018-07-16 22:18:43 -0700

    added user routes and updated todo routes to have authen paths

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const url = 'mongodb://steph:password123@ds137651.mlab.com:37651/crud_practice';
-const todoController = require('./controllers/todo-controller')
+const todoController = require('./controllers/todo-controller');
+const userController = require('./controllers/usercontroller');
+const cookieParser = require('cookie-parser');
 
 // app config
 const app = express();
 const PORT = 3000;
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+//static route
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // home route
@@ -16,17 +21,23 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-// make post requests
-app.post('/todos', todoController.makeTodo);
+// Todo Routes
+const verifySession = userController.verify;
+app.post('/todos', verifySession, todoController.makeTodo);
+app.get('/todos', verifySession, todoController.displayTodos);
+app.patch('/todos', verifySession, todoController.updateTodo);
+app.delete('/todos', verifySession, todoController.deleteTodo);
 
-// get all todos
-app.get('/todos', todoController.displayTodos);
+//Auth
+app.post('/login', 
+  userController.login,
+  userController.startSession
+);
+app.post('/signup',
+  userController.signup,
+  userController.startSession
+);
 
-// update
-app.patch('/todos', todoController.updateTodo);
-
-// delete
-app.delete('/todos', todoController.deleteTodo);
 
 // connect to mongoose
 mongoose.connect(url, () => console.log('connected to mongoose'));
